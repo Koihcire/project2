8cacfb7f3db695ca401dd0fd05a76a7d25df2190
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-07-17 17:23:43 +0800

    added card content to mytoolcard

--- project2-session-tools/src/ToolCard.js
+++ project2-session-tools/src/ToolCard.js
@@ -15,7 +15,49 @@ export default function ToolCard(props) {
                             <h4>{props.activeToolData.name}</h4>
                         </div>
                         <div className="myModal-body">
-                            This is a modal test
+                            <div>
+                                Created By: {props.activeToolData.createdBy.userName}
+                            </div>
+                            <div>
+                                Date Created: {props.activeToolData.dateCreated}
+                            </div>
+                            <div>
+                                Description: {props.activeToolData.description}
+                            </div>
+                            <div>
+                                Likes: {props.activeToolData.likes} *heart*
+                            </div>
+                            <div>
+                                Tags: {props.activeToolData.tags.map(tags => (
+                                    tags
+                                ))}
+                            </div>
+                            <div>
+                                Group Size: {props.activeToolData.groupSize.map(g => (
+                                    g
+                                ))}
+                            </div>
+                            <div>
+                                Time Needed: {props.activeToolData.timeNeeded} minutes
+                            </div>
+                            <div>
+                                Learning Objectives: {props.activeToolData.learningObjectives}
+                            </div>
+                            <div>
+                                Materials: {props.activeToolData.materials}
+                            </div>
+                            <div>
+                                Instructions:
+                                <div>
+                                    {parse(props.activeToolData.instructions)}
+                                </div>
+                            </div>
+                            <div>
+                                Debrief:
+                                <div>
+                                    {parse(props.activeToolData.debrief)}
+                                </div>
+                            </div>
                         </div>
                         <div className="myModal-footer">
                             <button onClick={props.closeToolCard}>close</button>
