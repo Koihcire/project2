17ea795b39fe000c4d30fc89fdac3c842bc7ffff
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-07-23 21:33:19 +0800

    added clear search filter button

--- project2-session-tools/src/Search.js
+++ project2-session-tools/src/Search.js
@@ -52,6 +52,19 @@ export default class Search extends React.Component {
         })
     }
 
+    clearSearchFilters = () => {
+        this.setState({
+            searchName: "",
+            searchRecent: "",
+            minTimeNeeded: "",
+            maxTimeNeeded: "",
+            tags: [],
+            groupSize: [],
+            difficulty: "",
+            sortBy: "recentlyAdded",
+        })
+    }
+
     submitComment = async () => {
         //check for username error
         if (!this.state.commentUserName || this.state.commentUserName.length > 100) {
@@ -293,8 +306,8 @@ export default class Search extends React.Component {
         }
     }
 
-    keyUpSearch = (e)=>{
-        if (e.key === "Enter"){
+    keyUpSearch = (e) => {
+        if (e.key === "Enter") {
             this.search();
         }
     }
@@ -339,11 +352,15 @@ export default class Search extends React.Component {
                             <div id="searchBox">
                                 <div className="container-fluid d-flex">
                                     <input id="searchField" name="searchName" type="text" className="form-control" value={this.state.searchName} onChange={this.updateFormField} placeholder="Search by Activity Name" onKeyUp={this.keyUpSearch} />
+                                    <button id="btnSearch" className="btn btn-sm" onClick={this.search}><svg xmlns="http://www.w3.org/2000/svg" width="25" height="30" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                                    </svg></button>
                                     <button id="btnFilter" className="btn btn-sm" onClick={this.toggleFilters}><svg xmlns="http://www.w3.org/2000/svg" width="25" height="30" fill="currentColor" class="bi bi-funnel" viewBox="0 0 16 16">
                                         <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z" />
                                     </svg></button>
-                                    <button id="btnSearch" className="btn btn-sm" onClick={this.search}><svg xmlns="http://www.w3.org/2000/svg" width="25" height="30" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                                    <button id="btnFilter" className="btn btn-sm" onClick={this.clearSearchFilters}><svg xmlns="http://www.w3.org/2000/svg" width="25" height="30" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
                                     </svg></button>
                                 </div>
 
