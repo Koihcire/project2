0956a73fe753a8bfa986480eee2cf9562c4ffa34
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-07-24 18:47:26 +0800

    added logo on navbar

--- project2-session-tools/src/HomePage.js
+++ project2-session-tools/src/HomePage.js
@@ -6,6 +6,7 @@ import LandingPage from "./LandingPage";
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./HomePage.css"
 import "./index.css"
+import logo from "./images/company-logo.png"
 
 
 export default class HomePage extends React.Component {
@@ -43,11 +44,15 @@ export default class HomePage extends React.Component {
                 <div id="main">
                     <div id="nav-bar" className="container-fluid">
                         <ul className="nav nav-tabs navbar-expand" >
+                            <li><svg xmlns="http://www.w3.org/2000/svg" fill="white" class="bi bi-briefcase-fill company-logo" viewBox="0 0 16 16">
+                                <path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v1.384l7.614 2.03a1.5 1.5 0 0 0 .772 0L16 5.884V4.5A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1h-3zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5z" />
+                                <path d="M0 12.5A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5V6.85L8.129 8.947a.5.5 0 0 1-.258 0L0 6.85v5.65z" />
+                            </svg></li>
                             <li className="nav-item">
                                 <div className={"nav-link" + (this.state.active === "landingPage" ? " active" : "")} aria-current="page" onClick={() => this.changePage("landingPage")}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
                                         <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z" />
-                                    </svg> <span class="navTitle">Home</span>
+                                    </svg> <span className="navTitle">Home</span>
                                 </div>
                             </li>
                             <li className="nav-item">
